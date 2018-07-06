9db405a816bc87fbf851504744cbece1d65705d1
Eric Johnson <ejohn156@uncc.edu>
2018-07-06 11:07:18 -0400

    cleaned up js

--- twoServers.js
+++ twoServers.js
@@ -38,19 +38,12 @@ inquirer.prompt([
         
     }
     
-    // Use the Node HTTP package to create our server.
-    // Pass the handleRequest function to empower it with functionality.
     var server1 = http.createServer(handleRequest1);
     var server2 = http.createServer(handleRequest2);
-    // Start our server so that it can begin listening to client requests.
     server1.listen(PORT1, function() {
-    
-      // Log (server-side) when our server has started
       console.log("Server listening on: http://localhost:" + PORT1);
     });
     server2.listen(PORT2, function() {
-    
-        // Log (server-side) when our server has started
         console.log("Server listening on: http://localhost:" + PORT2);
       });
     
